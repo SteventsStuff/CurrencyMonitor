37de5e0ec69090d4ef5406e3543347dacbdfbf8c
Volodymyr Miroshnycehnko <volodymyr.miroshnychenko@chromeriver.com>
2020-11-08 09:10:46 +0200

    updated tests for for 'arguments_handler'

--- test/utils/handlers/test_argumets_handler.py
+++ test/utils/handlers/test_argumets_handler.py
@@ -1,7 +1,7 @@
 import argparse
 import unittest
 
-from app.utils.handlers.argumets_handler import ArgumentsParser
+from app.utils.handlers.arguments_handler import ArgumentsParser
 
 
 class TestArgumentsParser(unittest.TestCase):
@@ -11,16 +11,6 @@ class TestArgumentsParser(unittest.TestCase):
 
     def test_init(self):
         self.assertIsInstance(self.argument_parser.parser, argparse.ArgumentParser)
-
-    def test_get_args(self):
-        """
-        Context: for current tests there is no actual program args past,
-        just checking default value of "config_path" argument
-        """
-        args = self.argument_parser.get_args()
-
-        self.assertIsInstance(args, argparse.Namespace)
-        self.assertEqual(args.config_path, '')
 
 
 if __name__ == '__main__':
